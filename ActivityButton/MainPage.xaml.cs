90e0d9fc3fa6bb8fc61f7ad9a5071ea8df2f5df6
Sergiu Cosmin <warborg@gmail.com>
2020-03-23 18:22:53 +0200

    Initial check-in of module ActivityButton

---
+++ ActivityButton/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ActivityButton
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PushModalAsync(new NavigationPage(new NewPage()));
+        }
+    }
+}
